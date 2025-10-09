8dee673e17d8b982699c2ef4554b26b24faeee0f
Becks723 <1991737469@qq.com>
2025-10-09 14:26:05 +0800

    feat: lab2 crawl_bili 0.2.0

--- lab2/crawl_bili/main.go
+++ lab2/crawl_bili/main.go
@@ -7,12 +7,22 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"net/http"
 	"net/url"
 	"strconv"
 	"time"
 )
 
+const (
+	oid string = "115206004939189"
+	//oid          string = "420981979"
+	type_        string = "1"
+	web_location string = "1315875"
+	ps           int    = 10
+)
+
+/* 一页的评论数据 */
 type replyData struct {
 	Data struct {
 		Cursor struct {
@@ -24,6 +34,7 @@ type replyData struct {
 	}
 }
 
+/* 单个评论 */
 type reply struct {
 	Content struct {
 		Message string
@@ -31,18 +42,33 @@ type reply struct {
 	Member struct {
 		Uname string
 	}
+	Rcount   int    // 子评论数量
+	Rpid_str string // 子评论的root，该字段用于请求子评论url
+}
+
+/* 某个一级评论下所有的子评论数据 */
+type subReplyData struct {
+	Data struct {
+		Replies []reply
+	}
 }
 
 func main() {
 	offset := ""
-	for i := 1; i <= 5; i++ {
-		fmt.Printf("正在爬取第 %d 页的评论\n", i)
-
+	p := 1
+	for {
+		fmt.Printf("正在爬取第 %d 页的评论\n", p)
+		p++
 		offset = crawl(offset)
 		time.Sleep(2 * time.Second)
+
+		if offset == "" {
+			break
+		}
 	}
 }
 
+/* 爬取一个评论页。若返回为空，则说明为最后一页 */
 func crawl(offset string) (nextOffset string) {
 	unix := time.Now().Unix()
 	nowtime := strconv.FormatInt(unix, 10)
@@ -51,15 +77,15 @@ func crawl(offset string) (nextOffset string) {
 	baseUrl := "https://api.bilibili.com/x/v2/reply/wbi/main"
 
 	params := url.Values{}
-	params.Set("oid", "420981979")
-	params.Set("type", "1")
+	params.Set("oid", oid)
+	params.Set("type", type_)
 	params.Set("mode", "3")
 	params.Set("pagination_str", pagination_str)
 	params.Set("plat", "1")
 	if offset == "" {
 		params.Set("seek_rpid", "")
 	}
-	params.Set("web_location", "1315875")
+	params.Set("web_location", web_location)
 	params.Set("w_rid", w_rid)
 	params.Set("wts", nowtime)
 
@@ -82,7 +108,7 @@ func crawl(offset string) (nextOffset string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", body)
+	//fmt.Printf("%s", body)
 
 	var rd replyData
 	if err := json.Unmarshal(body, &rd); err != nil {
@@ -91,25 +117,73 @@ func crawl(offset string) (nextOffset string) {
 
 	for i, r := range rd.Data.Replies {
 		fmt.Printf("%d: user: %s, comment: %s\n", i, r.Member.Uname, r.Content.Message)
+
+		pcount := ceil(float64(r.Rcount) / float64(ps))
+		for pn := 1; pn <= pcount; pn++ {
+			fmt.Printf("正在爬取第 %d 页的子评论……\n", pn)
+			crawlChildren(r.Rpid_str, pn)
+			time.Sleep(1 * time.Second)
+		}
 	}
 
 	nextOffset = rd.Data.Cursor.Pagination_reply.Next_offset
 	return
 }
 
+func crawlChildren(root string, pn int) {
+	baseUrl := "https://api.bilibili.com/x/v2/reply/reply"
+
+	params := url.Values{}
+	params.Set("oid", oid)
+	params.Set("type", type_)
+	params.Set("root", root)
+	params.Set("ps", strconv.Itoa(ps))
+	params.Set("pn", strconv.Itoa(pn))
+	params.Set("web_location", "333.788")
+
+	fullUrl := fmt.Sprintf("%s?%s", baseUrl, params.Encode())
+
+	req, err := http.NewRequest("GET", fullUrl, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36")
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var srd subReplyData
+	if err := json.Unmarshal(body, &srd); err != nil {
+		log.Fatal(err)
+	}
+
+	for i, r := range srd.Data.Replies {
+		fmt.Printf("    %d: user: %s, comment: %s\n", i, r.Member.Uname, r.Content.Message)
+	}
+}
+
 func getSign(offset string, nowtime string) (w_rid string, pagination_str string) {
 	pagination_str = fmt.Sprintf(`{"offset":"%s"}`, offset)
 
 	params := url.Values{}
-	params.Set("oid", "420981979")
-	params.Set("type", "1")
+	params.Set("oid", oid)
+	params.Set("type", type_)
 	params.Set("mode", "3")
 	params.Set("pagination_str", pagination_str)
 	params.Set("plat", "1")
 	if offset == "" {
 		params.Set("seek_rpid", "")
 	}
-	params.Set("web_location", "1315875")
+	params.Set("web_location", web_location)
 	params.Set("wts", nowtime)
 	v := params.Encode()
 	a := "ea1db124af3c7062474693fa704f4ff8"
@@ -120,4 +194,8 @@ func getSign(offset string, nowtime string) (w_rid string, pagination_str string
 func getMD5Hash(s string) string {
 	hash := md5.Sum([]byte(s))
 	return hex.EncodeToString(hash[:])
+}
+
+func ceil(num float64) int {
+	return int(math.Ceil(num))
 }
