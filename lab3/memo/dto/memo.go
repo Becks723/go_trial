207ae7c3dfdb2bdf36a44cdc444a70acc7b64c49
Becks723 <1991737469@qq.com>
2025-10-22 00:45:51 +0800

    fix: lab3 add binding tag for dto.

--- lab3/memo/dto/memo.go
+++ lab3/memo/dto/memo.go
@@ -15,14 +15,14 @@ type MemoData struct {
 }
 
 type AddMemoReq struct {
-	Title    string     `form:"title"`
-	Content  string     `form:"content"`
-	StartsAt *time.Time `form:"starts_at"` // RFC3339: 2000-01-01T23:59:59Z
-	EndsAt   *time.Time `form:"ends_at"`
+	Title    string     `form:"title" binding:"required"`
+	Content  string     `form:"content" binding:"required"`
+	StartsAt *time.Time `form:"starts_at" binding:"required"` // RFC3339: 2000-01-01T23:59:59Z
+	EndsAt   *time.Time `form:"ends_at" binding:"required"`
 }
 
 type UpdateMemoReq struct {
-	Id       uint             `form:"id"`
+	Id       uint             `form:"id" binding:"required"`
 	Title    string           `form:"title"`
 	Content  string           `form:"content"`
 	Status   model.MemoStatus `form:"status"`
