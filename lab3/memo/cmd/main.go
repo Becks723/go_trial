3a1a058fcb9632818b4c33dd31494b9599684573
Becks723 <1991737469@qq.com>
2025-10-22 17:49:32 +0800

    feat: lab3 support swagger docs.

--- lab3/memo/cmd/main.go
+++ lab3/memo/cmd/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"memo/controller"
+	_ "memo/docs"
 	"memo/middleware"
 	"memo/repository"
 	"memo/service"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title Memo API
+// @version 1.0
+// @description This is a memo api trial.
+// @host localhost:8080
+// @BasePath /api/v1
 func main() {
 	repository.Load()
 
@@ -28,6 +36,7 @@ func newController() *controller.Controller {
 
 func initializeRouter(c *controller.Controller) *gin.Engine {
 	router := gin.Default()
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	v1 := router.Group("/api/v1")
 	{
 		v1.POST("/user/signup", c.UserSignup)
--- lab3/memo/controller/controller.go
+++ lab3/memo/controller/controller.go
@@ -23,6 +23,18 @@ func NewController(userServ *service.UserService, memoServ *service.MemoService)
 	}
 }
 
+// UserSignup godoc
+// @Summary 用户注册
+// @Description 用户注册
+// @Tags user
+// @Accept x-www-form-urlencoded
+// @Produce json
+// @Param username formData string true "用户名"
+// @Param password formData string true "密码"
+// @Success 200 {object} dto.Response
+// @Failure 400 {object} dto.Response
+// @Failure 500 {object} dto.Response
+// @Router /user/signup [post]
 func (c *Controller) UserSignup(ctx *gin.Context) {
 	var req dto.SignupReq
 	if err := ctx.ShouldBind(&req); err == nil {
@@ -38,6 +50,18 @@ func (c *Controller) UserSignup(ctx *gin.Context) {
 	}
 }
 
+// UserLogin godoc
+// @Summary 用户登录
+// @Description 用户登录
+// @Tags user
+// @Accept x-www-form-urlencoded
+// @Produce json
+// @Param username formData string true "用户名"
+// @Param password formData string true "密码"
+// @Success 200 {object} dto.Response
+// @Failure 400 {object} dto.Response
+// @Failure 500 {object} dto.Response
+// @Router /user/login [post]
 func (c *Controller) UserLogin(ctx *gin.Context) {
 	var req dto.LoginReq
 	if err := ctx.ShouldBind(&req); err == nil {
@@ -53,6 +77,21 @@ func (c *Controller) UserLogin(ctx *gin.Context) {
 	}
 }
 
+// MemoAdd godoc
+// @Summary 创建代办事项
+// @Description 创建代办事项
+// @Tags memo
+// @Accept x-www-form-urlencoded
+// @Produce json
+// @Param title formData string true "标题"
+// @Param content formData string true "正文"
+// @Param starts_at formData string true "开始时间，格式2000-01-01T23:59:59Z"
+// @Param ends_at formData string true "截止时间，格式2000-01-01T23:59:59Z"
+// @Header 200 {string} Authorization "必需，登录校验字段"
+// @Success 200 {object} dto.Response
+// @Failure 400 {object} dto.Response
+// @Failure 500 {object} dto.Response
+// @Router /memo/add [post]
 func (c *Controller) MemoAdd(ctx *gin.Context) {
 	var req dto.AddMemoReq
 	if err := ctx.ShouldBind(&req); err != nil {
@@ -78,6 +117,23 @@ func (c *Controller) MemoAdd(ctx *gin.Context) {
 	ctx.JSON(e.Success, resp)
 }
 
+// MemoUpdate godoc
+// @Summary 更新代办事项
+// @Description 更新代办事项
+// @Tags memo
+// @Accept x-www-form-urlencoded
+// @Produce json
+// @Param id formData uint true "代办事项id"
+// @Param title formData string false "标题"
+// @Param content formData string false "正文"
+// @Param starts_at formData *time.Time false "开始时间，格式2000-01-01T23:59:59Z"
+// @Param ends_at formData *time.Time false "截止时间，格式2000-01-01T23:59:59Z"
+// @Param status formData model.MemoStatus false "状态"
+// @Header 200 {string} Authorization "必需，登录校验字段"
+// @Success 200 {object} dto.Response
+// @Failure 400 {object} dto.Response
+// @Failure 500 {object} dto.Response
+// @Router /memo/update [post]
 func (c *Controller) MemoUpdate(ctx *gin.Context) {
 	var req dto.UpdateMemoReq
 	if err := ctx.ShouldBind(&req); err != nil {
@@ -103,6 +159,20 @@ func (c *Controller) MemoUpdate(ctx *gin.Context) {
 	ctx.JSON(e.Success, resp)
 }
 
+// MemoList godoc
+// @Summary 查询代办事项
+// @Description 查询代办事项
+// @Tags memo
+// @Produce json
+// @Param limit query int false "分页容量"
+// @Param ps query int false "起始页码"
+// @Param pe query int false "终止页码"
+// @Param filter query dto.ListFilter false "筛选条件"
+// @Header 200 {string} Authorization "必需，登录校验字段"
+// @Success 200 {object} dto.Response
+// @Failure 400 {object} dto.Response
+// @Failure 500 {object} dto.Response
+// @Router /memo/list [get]
 func (c *Controller) MemoList(ctx *gin.Context) {
 	var params dto.ListMemoParams
 	if err := ctx.ShouldBindQuery(&params); err != nil {
@@ -128,6 +198,20 @@ func (c *Controller) MemoList(ctx *gin.Context) {
 	ctx.JSON(e.Success, resp)
 }
 
+// MemoSearch godoc
+// @Summary 搜索代办事项
+// @Description 搜索代办事项
+// @Tags memo
+// @Produce json
+// @Param limit query int false "分页容量"
+// @Param ps query int false "起始页码"
+// @Param pe query int false "终止页码"
+// @Param keywords query string true "必需，搜索关键词"
+// @Header 200 {string} Authorization "必需，登录校验字段"
+// @Success 200 {object} dto.Response
+// @Failure 400 {object} dto.Response
+// @Failure 500 {object} dto.Response
+// @Router /memo/search [get]
 func (c *Controller) MemoSearch(ctx *gin.Context) {
 	var params dto.SearchMemoParams
 	if err := ctx.ShouldBindQuery(&params); err != nil {
@@ -153,6 +237,19 @@ func (c *Controller) MemoSearch(ctx *gin.Context) {
 	ctx.JSON(e.Success, resp)
 }
 
+// MemoDelete godoc
+// @Summary 删除代办事项
+// @Description 删除代办事项。可删除指定id的事项，或者批量删除指定筛选条件的事项。
+// @Tags memo
+// @Accept x-www-form-urlencoded
+// @Produce json
+// @Param id formData uint true "代办事项id"
+// @Param filter formData dto.DeleteFilter true "筛选条件"
+// @Header 200 {string} Authorization "必需，登录校验字段"
+// @Success 200 {object} dto.Response
+// @Failure 400 {object} dto.Response
+// @Failure 500 {object} dto.Response
+// @Router /memo/delete [post]
 func (c *Controller) MemoDelete(ctx *gin.Context) {
 	// retrieve current user id
 	var uid uint
